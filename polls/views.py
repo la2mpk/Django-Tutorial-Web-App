777f7d437b3a936a278f3bf432f9bc60857f2fee
la2m <la2mdjango@gmail.com>
2020-07-12 15:34:41 -0500

    Django WebApp Tutorial Parte 3

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.http import HttpResponse
+from .models import Question
 
 
-class IndexPageView(generic.base.TemplateView):
-
+class IndexPageView(generic.ListView):
     template_name = 'polls/index.html'
+    context_object_name = 'latest_questions_list'
+
+    def get_queryset(self):
+        queryset = Question.objects.order_by('-pub_date')[:5]
+        return queryset
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/details.html', context)
+
+
+def results(request, question_id):
+    return HttpResponse(f'You are looking at the results of question {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'You are voting on question {question_id}')
